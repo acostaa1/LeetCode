039f25837e4f8f1b2b93fa887c9a8366faf8bac7
Angel Acosta <38592434+acostaa1@users.noreply.github.com>
2022-08-01 21:31:25 -0400

    Time: 215 ms (44.58%), Space: 56.3 MB (9.82%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+
+const groupAnagrams = (arr) => {
+  
+  const result = [];
+  const store = {}; // keep track of letters of each word
+  for (let word of arr) {
+    let sorted = word.split('').sort().join('')
+    if (!(sorted in store)) {
+      store[sorted] = [sorted] || []; //[sorted] || []
+    }
+    store[sorted].push(word);//store[sorted].push(word)
+  }
+  for(let val of Object.values(store)) {
+    //console.log(val)
+    if (val.length >= 2) {
+     result.push(val)
+   }
+  }
+  return result.map((a)=> a.splice(1, a.length -1));
+}
